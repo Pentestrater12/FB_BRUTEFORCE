515b1a50bd596ac7f5679f3fab7988a6b054ffcf
Pentestrater12 <88031493+Pentestrater12@users.noreply.github.com>
2021-08-06 22:51:03 +0800

    Create fb-bruteforce.py

---
+++ fb-bruteforce.py
@@ -0,0 +1,11 @@
+## FB_BRUTEFORCE - FACEBOOK BRUTEFORCE HACK PASSWORD
+# -*- coding:utf-8 -*-
+##
+import os
+import sys
+import urllib
+import hashlib
+
+API_SECRET = "62f8ce9f74b12f84c123cc23437a4a32"
+
+__banner__ = """
