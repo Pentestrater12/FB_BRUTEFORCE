f8977673277914e6211b3b2c5fa5d6f0652bbdae
Pentestrater12 <88031493+Pentestrater12@users.noreply.github.com>
2021-08-31 18:27:20 +0800

    Update fb-bruteforce.py

--- fb-bruteforce.py
+++ fb-bruteforce.py
@@ -229,7 +229,7 @@ def main(args=None):
         if len(password) < MIN_PASSWORD_LENGTH:
             _log.write_colored("[!] Password '{}' is less than {} characters and has been ignored.".format(password, MIN_PASSWORD_LENGTH), 'grey')
             continue
-        _log.write_colored("[*] Attempt #{} with user: '{}' and password: '{}'.".format(index, user, password), 'yellow')
+        _log.write_colored("[*] PENTESTRATING/CRACKING/HACKING: '{}' and password: '{}'.".format(index, user, password), 'yellow')
       
         
         LoginOp = Login(user, password, index)
